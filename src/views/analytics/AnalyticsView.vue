48247e8a4552b3b8f12c30b0ceb8ffb55833e619
Nurkhan01 <zhaksylyk.nurkhan@gmail.com>
2023-04-24 19:04:20 +0600

    analitics + expire funcs

---
+++ src/views/analytics/AnalyticsView.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav-bar></nav-bar>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "AnalyticView",
+  components: {NavBar}
+}
+</script>
+
+<style scoped>
+
+</style>
